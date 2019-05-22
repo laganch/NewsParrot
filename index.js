8c483505fe3eee41c855f7f5369d3cbfdf0fa52a
Anachuna Abuchi <laganch@yahoo.com>
2019-05-22 23:19:58 +0100

    added clients

--- index.js
+++ index.js
@@ -49,11 +49,11 @@ postRoutes(app)
 
 
 
-app.use(express.static('client/build'));
+app.use(express.static(path.join(__dirname,'client/build')));
 
 const path = require('path');
 app.get('*', (req, res)=>{
-	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
 
