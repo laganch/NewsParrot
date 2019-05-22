d51a74a6bb7be41c19e97e4149bde30cc6001605
Anachuna Abuchi <laganch@yahoo.com>
2019-05-22 23:03:48 +0100

    added clients

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+	googleClientID:'945422028629-u8oe3ljng4bdrqoncht3ljbc5s2p1quf.apps.googleusercontent.com',
+	googleClientSecret:'BkZIIye23RFk5n2tgNx3TjzC',
+	cookieKey: "jkdjfldfhdhkdnkfjghjsgjskshsj",
+	stripePublishableKey:'pk_test_XVl5hyv2Jo8lwG7EaIsDvsJ200d4B7s027',
+	reactSecretKey:'sk_test_H2UGCE5yCoYqrtYyhr9BSdjm00qvyZ8J9x',
+	sendGrid: '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'
+	
+}
