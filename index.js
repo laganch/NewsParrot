69ca7c3633d4f78ba786a806103a1efb6d3c853d
Anachuna Abuchi <laganch@yahoo.com>
2019-05-23 07:30:38 +0100

    last

--- index.js
+++ index.js
@@ -49,11 +49,11 @@ postRoutes(app)
 
 
 
-const path = require('path');
-app.use(express.static(path.join(__dirname,'client/build')));
+app.use(express.static('client/build'));
 
+const path = require('path');
 app.get('*', (req, res)=>{
-	res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
 
