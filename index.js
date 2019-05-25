e686304b9f815f2ea1ae7a077e044783c87e61f6
Anachuna Abuchi <laganch@yahoo.com>
2019-05-25 10:03:31 +0100

    all

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const express 			= require("express");
-const mongoose 			= require("mongoose");
-const passportLocalMongoose    = require("passport-local-mongoose"),
-    LocalStrategy = require('passport-local');
-const cookieSession 	= require('cookie-session');
-const passport 			= require('passport');
-const authRouth 		= require('./routes/authRoute');
-const postRoutes 		= require('./routes/postRoutes');
-const billingRoutes 		= require('./routes/billingRoutes');
-const surveyRoutes 		= require('./routes/surveyRoutes');
-const bodyParser 		= require('body-parser');
-// const expressSession     = require("express-session");
-const keys 				=require("./config/keys");
-						require('./models/user');
-						require('./models/advert');
-						require('./models/survey');
-						require('./service/passport');
-						require('./service/passportLocal');
+const express 					= require("express");
+const mongoose 					= require("mongoose");
+const passportLocalMongoose     = require("passport-local-mongoose"),
+    LocalStrategy 				= require('passport-local');
+const cookieSession 			= require('cookie-session');
+const passport 					= require('passport');
+const authRouth 				= require('./routes/authRoute');
+const postRoutes 				= require('./routes/postRoutes');
+const billingRoutes 			= require('./routes/billingRoutes');
+const surveyRoutes 				= require('./routes/surveyRoutes');
+const path 						= require('path');
+const bodyParser 				= require('body-parser');
+const keys 						=require("./config/keys");
+								require('./models/user');
+								require('./models/advert');
+								require('./models/survey');
+								require('./service/passport');
+								require('./service/passportLocal');
 
 
 mongoose.connect("mongodb://laganch:wisdom4190@ds119662.mlab.com:19662/cryptoapp");
@@ -51,7 +51,6 @@ postRoutes(app)
 
 
 
-const path = require('path');
 app.get('*', (req, res)=>{
 	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
